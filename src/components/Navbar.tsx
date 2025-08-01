cd0a6080c9f542b64ea6639219f0b378157ff359
wiliami <wiliaup97@gmail.com>
2025-08-01 14:46:26 -0400

    update button require account

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -127,9 +127,11 @@ export const Navbar = () => {
           </nav>
             
           <div className="hidden md:flex gap-2 items-center">
-            <a href="http://localhost:3000/login" className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-green-600 dark:hover:bg-green-400-700 focus:outline-none dark:focus:ring-blue-800">Login</a>
+            <a href="http://localhost:3000/login">Login</a>
 
-            <a href="#">Cadastro</a>
+            <a href="#" className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-green-600 dark:hover:bg-green-400-700 focus:outline-none dark:focus:ring-blue-800">
+              Solicitar acesso
+            </a>
             <ModeToggle />
 
           </div>
