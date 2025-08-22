fb910561cfb4dee2e01c57bdc4ff5413b98a7214
Braz Junior <90149109+brxzjunior@users.noreply.github.com>
2025-08-22 16:49:06 -0400

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -112,14 +112,14 @@ export const Footer = () => {
 
       <section className="container pb-14 text-center">
         <h3>
-          &copy; 2025 developed by{" "}
+          &copy; 2025 landing page made by{" "}
           <a
             rel="noreferrer noopener"
             target="_blank"
             href="https://www.linkedin.com/in/leopoldo-miranda/"
             className="text-primary transition-all border-primary hover:border-b-2"
           >
-            Performance Agency
+            Leo Miranda
           </a>
         </h3>
       </section>
